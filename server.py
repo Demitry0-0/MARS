430d743ca7b62d4ed721c641d86175a7922358bf
Demitry0-0 <58517416+Demitry0-0@users.noreply.github.com>
2020-05-01 18:31:29 +0300

    Add files via upload

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 from flask import Flask, url_for, request
+from PIL import Image
 
 app = Flask(__name__)
 
@@ -33,7 +34,9 @@ def image_mars():
                       </head>
                       <body>
                         <h1>Жди нас, Марс!</h1>
-                        <img src="https://ic.pics.livejournal.com/serzigzagser/74708980/1647453/1647453_original.jpg" alt="Сдесь должен быть сладенький марс">
+                      < img
+                        src = "https://ic.pics.livejournal.com/serzigzagser/74708980/1647453/1647453_original.jpg"
+                        alt = "Сдесь должен быть сладенький марс" >
                       </body>
                     </html>"""
 
@@ -211,6 +214,97 @@ def results(nickname, level, rating):
                         </div>
                     </body>
                     </html>"""
+
+
+@app.route('/carousel')
+def carousel():
+    return """<!doctype html>
+                    <html lang="ru">
+                      <head>
+                        <meta charset="utf-8">
+                        <title>Пейзажи Марса</title>
+                        <link rel="stylesheet" 
+    href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" 
+    integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" 
+    crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+                      </head>
+                      <body>
+                      <center><h1>Пейзажи Марса</h1></center>
+<div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img class="d-block w-100" src="static/img/mars.jpg" alt="Первый слайд">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="static/img/mars1.jpg" alt="Второй слайд">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="static/img/mars2.jpg" alt="Третий слайд">
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+                      </body>
+                    </html>"""
+
+
+@app.route('/sample_file_upload', methods=['POST', 'GET', 'PATCH'])
+def sample_file_upload():
+    print(request.files)
+    if request.files:
+        file = f'<img src={url_for("static", filename="photo.jpg")} alt="Такой фотки нет">'
+        # Image.open(request.files['file'].read()).save('photo.jpg')
+    else:
+        file = ''
+    print(request.files.get('file'))
+
+    print(request.method)
+    if request.method == 'PATCH':
+        return 'dsfdfsfdsdf'
+    elif request.method == 'GET':
+        print(request.files)
+        return f'''<!doctype html>
+                        <html lang="en">
+                          <head>
+                            <meta charset="utf-8">
+                            <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+                             <link rel="stylesheet"
+                             href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+                             integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh"
+                             crossorigin="anonymous">
+                            <link rel="stylesheet" type="text/css" href="{url_for('static', filename='css/style.css')}" />
+                            <title>Пример загрузки файла</title>
+                          </head>
+                          <body>
+                            <h1>Загрузим файл</h1>
+                            <form method="put" enctype="multipart/form-data">
+                               <div class="form-group" method="put">
+                                    <label for="photo">Выберите файл</label>
+                                    <input type="file" method="put" class="form-control-file" id="photo" name="file">
+                                </div>
+                                <button type="submit" class="btn btn-primary">Отправить</button>
+                                {file}
+                            </form>
+                          </body>
+                        </html>'''
+    elif request.method == 'POST':
+        f = request.files['file']
+        print(dir(request.files['file']))
+        print(request.files['file'].filename)
+        print(request.files)
+
+        # print(f.read())
+        return "Форма отправлена"
 
 
 if __name__ == '__main__':
