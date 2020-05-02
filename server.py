2560c9ecba4779d9229e8ae0b562aa693a157f0f
Demitry0-0 <58517416+Demitry0-0@users.noreply.github.com>
2020-05-02 19:35:42 +0300

    Add files via upload

--- server.py
+++ server.py
@@ -1,6 +1,34 @@
-from flask import Flask, url_for, request, render_template
+from flask import Flask, url_for, request, render_template, redirect
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, IntegerField
+from wtforms.validators import DataRequired
+
+
+class LoginForm(FlaskForm):
+    username = StringField('Логин', validators=[DataRequired()])
+    password = PasswordField('Пароль', validators=[DataRequired()])
+    remember_me = BooleanField('Запомнить меня')
+    submit = SubmitField('Войти')
+
+
+class DostupForm(FlaskForm):
+    id_astronaft = IntegerField('id астронавта')
+    password_astronaft = PasswordField('Пароль астронавта', validators=[DataRequired()])
+    id_captain = IntegerField('id капитана')
+    password_captain = PasswordField('Пароль капитана', validators=[DataRequired()])
+    submit = SubmitField('Доступ')
+
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = DostupForm()
+    if form.validate_on_submit():
+        return redirect('/success')
+    return render_template('login.html', title='Авторизация', form=form)
 
 
 @app.route('/')
